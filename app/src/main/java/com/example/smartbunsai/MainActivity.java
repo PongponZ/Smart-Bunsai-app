a4af9009ef3c2eaa13dfc40fec656325871401e8
PongponZ <mr.pongpon.sinlapa.m.m@gmail.com>
2020-05-10 21:09:12 +0700

    First upload

---
+++ app/src/main/java/com/example/smartbunsai/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.smartbunsai;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+    private LottieAnimationView lottieAnimationView;
+    private int SPLASH_TIME = 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, SPLASH_TIME);
+        lottieAnimationView = findViewById(R.id.lottie_tree);
+    }
+}
