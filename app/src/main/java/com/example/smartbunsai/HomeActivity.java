424d613da271c6c66e9455e38af2f275d08106a6
Atittan <pao22304.ar@gmail.com>
2020-05-20 11:09:42 +0700

    update and add arduino code

--- app/src/main/java/com/example/smartbunsai/HomeActivity.java
+++ app/src/main/java/com/example/smartbunsai/HomeActivity.java
@@ -2,13 +2,226 @@ package com.example.smartbunsai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
+import java.util.Timer;
 
 public class HomeActivity extends AppCompatActivity {
+    String TAG = "TAG";
+    //Firebase fireStore
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference  water_switch_auto = database.getReference("switch/water_auto/status");
+    DatabaseReference  water_switch = database.getReference("switch/water_switch/status");
+    Query light_qry = database.getReference("sensor/light/values").orderByKey().limitToLast(1);
+    Query temperature_qry = database.getReference("sensor/temperature/values").orderByKey().limitToLast(1);
+    Query moisture_qry = database.getReference("sensor/moisture/values").orderByKey().limitToLast(1);
+
+    String light_string, temperature_string, moisture_string;
+    TextView light_Text, temperature_Text, moisture_Text, status_Text;
+    ImageView img_watering, img_light, img_temperature, img_moisture;
+    Switch autoSwitch, autoSwitch2;
+    LinearLayout water_layout;
+
+    //value
+    boolean auto_switch = false, isOpen = false;
+    int moisture_value = 0;
+    int light_value = 0;
+    double temp_value = 0;
+    String WATER_AUTO_STATUS = "WATER_AUTO_STATUS";
+    SharedPreferences sp;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //set icon image
+        img_watering = findViewById(R.id.img_watering);
+        img_watering.setImageResource(R.drawable.wateringtool);
+
+        img_light = findViewById(R.id.img_light);
+        img_light.setImageResource(R.drawable.illumination);
+
+        img_temperature = findViewById(R.id.img_temp);
+        img_temperature.setImageResource(R.drawable.tempurature);
+
+        img_moisture = findViewById(R.id.img_moisture);
+        img_moisture.setImageResource(R.drawable.raindrops);
+
+        light_Text = findViewById(R.id.light_Text);
+        temperature_Text = findViewById(R.id.temperature_Text);
+        moisture_Text = findViewById(R.id.moisture_Text);
+
+        water_layout = findViewById(R.id.linear_layout_watering);
+        status_Text = findViewById(R.id.myStatus);
+
+        autoSwitch2 = findViewById(R.id.auto_switch);
+        //PREF
+        sp = getSharedPreferences(WATER_AUTO_STATUS, Context.MODE_PRIVATE);
+        editor = sp.edit();
+
+        if(!sp.contains("initialized")){
+            //Indicate that the default shared prefs have been set
+            editor.putBoolean("initialized", true);
+            //Set some default shared pref
+            editor.putBoolean("WATER_AUTO", false);
+            editor.commit();
+        }
+        if(sp.contains("WATER_AUTO")){
+            autoSwitch2.setChecked(sp.getBoolean("WATER_AUTO", false));
+            auto_switch = sp.getBoolean("WATER_AUTO", false);
+            if(auto_switch) water_layout.setBackgroundResource(R.drawable.btn_disable);
+
+
+        }
+
+        fetchData();
+
+        //Watering Button
+        img_watering.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!auto_switch){
+                    if(!isOpen){
+                        water_switch.setValue(true);
+                        water_layout.setBackgroundResource(R.drawable.btn_on);
+                        isOpen = true;
+                    }else{
+                        water_layout.setBackgroundResource(R.drawable.btn_shape_xlong);
+                        water_switch.setValue(false);
+                        isOpen = false;
+
+                    }
+
+                }else{
+                    Toast.makeText(getApplicationContext(), "WATERING IS AUTO",Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    public void autoSwitch(View view){
+        autoSwitch = findViewById(view.getId());
+        auto_switch = autoSwitch.isChecked();
+
+        //set value to realtime database
+        water_switch_auto.setValue(auto_switch);
+        if(auto_switch){
+            water_layout.setBackgroundResource(R.drawable.btn_disable);
+        }else{
+            water_layout.setBackgroundResource(R.drawable.btn_shape_xlong);
+        }
+
+        editor.putBoolean("WATER_AUTO", auto_switch);
+        editor.commit();
+    }
+
+    //Temp 15 – 40 องศาเซลเซียส
+    //Moisture ต้องการน้ำ < 1500 | > 2200 ไม่ต้องการน้ำ | > 3000 น้ำเยอะเกินไป
+
+    private void calculateStatus(){
+        String status, temp_string, moisture_string, light_string;
+
+        //check moisture
+        if(moisture_value < 1500) moisture_string = "เราต้องการน้ำ";
+        else if(moisture_value > 3000) moisture_string = "น้ำท่วมแล้วจ้าา";
+        else moisture_string = "ปริมาณน้ำเพียงพอแล้ว";
+
+        //check Temp
+        if(temp_value < 15) temp_string = "อากาศเย็นมากเลยแม่";
+        else if(temp_value > 40) temp_string = "อากาศร้อนมากเราจะตายแล้ว";
+        else temp_string = "อากาศพอดีเลย เย้";
+
+        //check light
+        //if(light_value > 4000) light_string="แสงเยอะมาก";
+
+        status = moisture_string + " " + temp_string;
+        status_Text.setText(status);
+    }
+
+    private void fetchData(){
+        // Read from the database
+        light_qry.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                String subString = map.values().toString();
+                int length = subString.length();
+                light_string = subString.substring(1,length-1);
+                light_Text.setText("Light: " + light_string);
+                light_value = Integer.valueOf(light_string);
+                calculateStatus();
+                Log.d(TAG, "Light Value is: " + map.values().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        temperature_qry.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                String subString = map.values().toString();
+                int length = subString.length();
+                temperature_string = String.format("%.2f", Float.valueOf(subString.substring(1,length-1)));
+                temperature_Text.setText("Temp: " + temperature_string);
+                temp_value = Double.valueOf(temperature_string);
+                calculateStatus();
+                Log.d(TAG, "temperature Value is: " + map.values().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+        moisture_qry.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+
+                String subString = map.values().toString();
+
+                int length = subString.length();
+                moisture_string = subString.substring(1,length-1);
+                moisture_Text.setText("Moisture: " + moisture_string);
+                moisture_value = Integer.valueOf(moisture_string);
+                calculateStatus();
+                Log.d(TAG, "moisture Value is: " + map.values().toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
--- arduino-sensors/Smart-Bonsai/Smart-Bonsai.ino
+++ arduino-sensors/Smart-Bonsai/Smart-Bonsai.ino
@@ -1,59 +1,82 @@
+//#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <MCP3208.h>
 #include <SPI.h>
 #include <FirebaseArduino.h>
-//#include <ArduinoJson.h>
 
 //set Microship up
 MCP3208 adc(D8);
 
-#define WIFI_SSID       "WiFi_NAME"
-#define WIFI_PASSWORD   "WiFi_Password"
-#define FIREBASE_HOST "Firebase_URL_HOST"
-#define FIREBASE_AUTH "Firbase_AUTH_KEY"
+#define WIFI_SSID       "Atittan"
+#define WIFI_PASSWORD   "kDywepao"
+#define FIREBASE_HOST "smart-bonsai-f6d63.firebaseio.com"
+#define FIREBASE_AUTH "xiSAEgG6d2oC8vMoW08911mAbm6JOJt9douyAA6G"
 #define water_swicth D1
 
-int temp_sensor_pin = 0, light_sensor_pin = 3, moisture_sensor_pin = 6, calibrate = 53;
+const int temp_sensor_pin = 0, light_sensor_pin = 3, moisture_sensor_pin = 6;
+int calibrate = 85;
+bool auto_switch = false, sw_status = false;
+float moisture;
 
 void setup() {
-  adc.begin();                                      //Microship read
-  Serial.begin(115200);
-  
+  adc.begin();                                      //Microship read begin
+  Serial.begin(115200);                             //baud rate
   connectWiFi(WIFI_SSID, WIFI_PASSWORD);            //Connect to WiFi
-  
-  pinMode(water_swicth, OUTPUT);
-  
+  pinMode(water_swicth, OUTPUT);                    //Set pinMode for Selenoid Valve
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);     //Connect to Firebase
-  Firebase.stream("water_switch/status");           //Check status of switch
+  Firebase.stream("/switch");                       //Detect Swtitch
+  Firebase.setBool("switch/water_auto/status", auto_switch);
+  Firebase.setBool("switch/water_switch/status", sw_status);
 }
 
 void loop() {
-  
+  int rawValue = adc.analogRead(temp_sensor_pin);
+  float light_analog_val = adc.analogRead(light_sensor_pin);
+  float moisture_analog_val = adc.analogRead(moisture_sensor_pin);
+  float temperature_analog_val = convertTemperature(rawValue);
+    
+  pushFirebase("temperature",temperature_analog_val);
+  pushFirebase("moisture",moisture_analog_val);
+  pushFirebase("light",light_analog_val);
   switchCheck();
-  
-  int rawValue = adc.analogRead(temp_sensor_pin);                       //Raw Data
-  float light_analog_val = adc.analogRead(light_sensor_pin);            //Raw Data
-  float moisture_analog_val = adc.analogRead(moisture_sensor_pin);      //Raw Data
-  float temperature_analog_val = convertTemperature(rawValue);          //Raw Data
-  
-  //Sent to Firebase
-//sendObject("temperature",temperature_analog_val);
-  pushFloat("temperature",temperature_analog_val);
-  Serial.println("Temperature: "+ String(temperature_analog_val));
-  
-//sendObject("moisture",moisture_analog_val);
-  pushFloat("moisture",moisture_analog_val);
-  Serial.println("Moisture: "+ String(moisture_analog_val));
-  
-//sendObject("light",light_analog_val);
-  pushFloat("light",light_analog_val);
-  Serial.println("Light: "+ String(light_analog_val)+"\n");
+  if(auto_switch == true){
+    moisture = moisture_analog_val;
+    waterAutoSwitch(moisture);
+  }else{
+    waterManualSwitch();
+  }
+  Serial.println();     
+  //sendObject("temperature",temperature_analog_val);
 
-  delay(15000);
+  Serial.println("auto_switch: "+String(auto_switch));
+  Serial.println("water_switch: "+String(sw_status));
 
-}//End Loop
+  delay(10000);
 
-//Check Switch
+   if(auto_switch == true && sw_status == true){                //if you turn on auto switch but 
+    Firebase.setBool("switch/water_switch/status", false);      //you forget to turn off the switch
+    sw_status = false;                                          //this will be turn of the water
+   }                                                            
+}//end Loop
+
+
+void waterAutoSwitch(float moisture){
+  if(moisture <= 1500.0){
+    digitalWrite(water_swicth, HIGH);
+    delay(180000);
+    digitalWrite(water_swicth, LOW);
+    Serial.println("Water was close!");
+  }else{
+    digitalWrite(water_swicth, LOW);
+    Serial.println("No need water!");
+  }
+}
+
+void waterManualSwitch(){
+  digitalWrite(water_swicth, (sw_status == false ? LOW : HIGH));    //Switch on/off
+  (sw_status == false ? Serial.println("Water already close") : Serial.println("Water!"));
+}        
+
 void switchCheck(){
   if (Firebase.failed()) {
     Serial.println("streaming error");
@@ -64,25 +87,32 @@ void switchCheck(){
      FirebaseObject event = Firebase.readEvent();
      String eventType = event.getString("type");
      eventType.toLowerCase();
-
+ 
      if (eventType == "") return ;
      Serial.print("event: ");
      Serial.println(eventType);
+     
      if (eventType == "put") {
        String path = event.getString("path");
        bool data = event.getBool("data");
        Serial.println("\n[" + path + "] " + String(data));
-       if (path == "/") {
-         digitalWrite(water_swicth, (data == false ? LOW : HIGH));    //if turn on in your phone Switch will be turn on
+       
+       if (path == "/water_switch/status") {
+         (data != true ? sw_status = false : sw_status = true);
        }
-     }
+       
+       if(path == "/water_auto/status") {
+         (data == true ? auto_switch = true : auto_switch = false);    //Detect Auto/Not
+
+       }//end Path
+     }//end Event
   }
-}
+}//end switchCheck()
 
 //Temperature Converter
 float convertTemperature(long volt) {
-  float milliVolt = volt * 3.3 / 4095.0 * 1000.0;
-  float tempC = (milliVolt - 500.0) / 10.0;
+  float milliVolt = (volt * 3.3 / 4095.0) * 1000.0;
+  float tempC = (milliVolt - 500.0) / 10.00;
   return tempC;
 }
 
@@ -103,9 +133,10 @@ void connectWiFi(String ssid, String pwd) {
 }
 
 //Send by Value No object
-void pushFloat(String sensor_name, float value){
+void pushFirebase(String sensor_name, float value){
   String path = "sensor/"+sensor_name+"/values";
   Firebase.pushFloat(path,value);
+  Serial.println(sensor_name + ": "+ String(value));
 }
 
 //Send by Object
